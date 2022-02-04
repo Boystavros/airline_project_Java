999afc3050da5d222e9dd7f98e68ffd08280e9d8
Sacha Ponniah <sponniah812@gmail.com>
2022-02-04 18:08:14 +0000

    Pilot and tests complete.

--- src/main/java/People/Pilot.java
+++ src/main/java/People/Pilot.java
@@ -1,4 +1,17 @@
 package People;
 
-public class Pilot {
+public class Pilot extends Staff{
+
+    private String licenceNum;
+
+    public Pilot(String name, Rank rank, String licenceNum) {
+        super(name, rank);
+        this.licenceNum = licenceNum;
+    }
+
+    public String getLicenceNum() {
+        return licenceNum;
+    }
+
+
 }
--- src/test/java/PilotTest.java
+++ src/test/java/PilotTest.java
@@ -1,2 +1,31 @@
-public class PilotTest {
+import People.Pilot;
+import People.Rank;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PilotTest{
+
+    private Pilot pilot;
+
+    @Before
+    public void before(){
+        this.pilot = new Pilot("Wilbur", Rank.CAPTAIN, "JN4567");
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Wilbur", pilot.getName());
+    }
+
+    @Test
+    public void canGetRank(){
+        assertEquals(Rank.CAPTAIN, pilot.getRank());
+    }
+
+    @Test
+    public void canGetLicenceNum(){
+        assertEquals("JN4567", pilot.getLicenceNum());
+    }
 }
