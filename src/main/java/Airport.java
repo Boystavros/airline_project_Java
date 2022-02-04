a9d0272f9556a551b0f48257aedb723fca9b0aa8
Sacha Ponniah <sponniah812@gmail.com>
2022-02-04 18:22:49 +0000

    Started Flight class.

---
+++ src/main/java/Airport.java
@@ -0,0 +1,7 @@
+public enum Airport {
+
+    EDI,
+    GLA,
+    LDN,
+    LAX;
+}
--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,19 @@
+import People.CabinCrewMember;
+import People.Passenger;
+import People.Pilot;
+
+import java.util.ArrayList;
+
 public class Flight {
+
+    private Pilot pilot;
+    private ArrayList<CabinCrewMember> crew;
+    private ArrayList<Passenger> passengers;
+    private Plane plane;
+    private String flightNum;
+    private Airport destination;
+
+
+
+
 }
