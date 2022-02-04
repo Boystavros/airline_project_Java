a1d76a4be4f2a58d77163ebb1fde29dc2c3d4213
Sacha Ponniah <sponniah812@gmail.com>
2022-02-04 19:41:20 +0000

    Flight class half complete.

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -12,8 +12,58 @@ public class Flight {
     private Plane plane;
     private String flightNum;
     private Airport destination;
+    private Airport departsFrom;
+    private String departureTime;
 
 
+    public Flight(Pilot pilot, Plane plane, String flightNum, Airport destination, Airport departsFrom, String departureTime) {
+        this.pilot = pilot;
+        this.crew = new ArrayList<>();
+        this.passengers = new ArrayList<>();
+        this.plane = plane;
+        this.flightNum = flightNum;
+        this.destination = destination;
+        this.departsFrom = departsFrom;
+        this.departureTime = departureTime;
+    }
 
+    public Pilot getPilot() {
+        return pilot;
+    }
 
+    public ArrayList<CabinCrewMember> getCrew() {
+        return crew;
+    }
+
+    public ArrayList<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public Plane getPlane() {
+        return plane;
+    }
+
+    public String getFlightNum() {
+        return flightNum;
+    }
+
+    public Airport getDestination() {
+        return destination;
+    }
+
+    public Airport getDepartsFrom() {
+        return departsFrom;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void addCrewMember(CabinCrewMember cabinCrewMember) {
+        this.crew.add(cabinCrewMember);
+    }
+
+    public void bookFLight(Passenger passenger) {
+        this.passengers.add(passenger);
+    }
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,91 @@
+import People.CabinCrewMember;
+import People.Passenger;
+import People.Pilot;
+import People.Rank;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
 public class FlightTest {
+
+    private Flight flight;
+    private Pilot pilot;
+    private CabinCrewMember cabinCrewMember1;
+    private CabinCrewMember cabinCrewMember2;
+    private Passenger passenger1;
+    private Passenger passenger2;
+    private Plane plane;
+    private String flightNum;
+    private Airport destination;
+    private Airport departsFrom;
+    private String departureTime;
+
+    @Before
+    public void before(){
+        this.pilot = new Pilot("Sacha", Rank.CAPTAIN, "JN4567");
+        this.plane = new Plane(PlaneType.BOEING747);
+        this.flightNum = "ED123";
+        this.destination = Airport.EDI;
+        this.departsFrom = Airport.LAX;
+        this.departureTime = "0540";
+        this.flight = new Flight(pilot, plane, flightNum, destination, departsFrom, departureTime);
+        this.cabinCrewMember1 = new CabinCrewMember("Teddy", Rank.FIRST_OFFICER);
+        this.cabinCrewMember2 = new CabinCrewMember("Evie", Rank.PURSER);
+        this.passenger1 = new Passenger("Fiona", 1);
+        this.passenger2 = new Passenger("Wilbur", 2);
+    }
+
+    @Test
+    public void testGetPilot() {
+        assertEquals("JN4567", flight.getPilot().getLicenceNum());
+    }
+
+    @Test
+    public void crewListStartsEmpty(){
+        assertEquals(0, flight.getCrew().size());
+    }
+
+    @Test
+    public void testAddCrewMember() {
+        flight.addCrewMember(cabinCrewMember1);
+        flight.addCrewMember(cabinCrewMember2);
+        assertEquals(2, flight.getCrew().size());
+    }
+
+    @Test
+    public void testPassengerListStartsEmpty() {
+        assertEquals(0, flight.getPassengers().size());
+    }
+
+    @Test
+    public void testAddPassenger(){
+        flight.bookFLight(passenger1);
+        assertEquals(1, flight.getPassengers().size());
+    }
+
+    @Test
+    public void testGetPlane() {
+        assertEquals(PlaneType.BOEING747, flight.getPlane().getType());
+    }
+
+    @Test
+    public void testGetFlightNum() {
+        assertEquals("ED123", flight.getFlightNum());
+    }
+
+    @Test
+    public void testGetDestination() {
+    }
+
+    @Test
+    public void testGetDepartsFrom() {
+    }
+
+    @Test
+    public void testGetDepartureTime() {
+    }
 }
